e12e119a448c325be8021f7b0d01414aefb9dcf2
PaulWang <13126588733@163.com>
2023-03-02 23:09:15 +0800

    Update 27_removeElement.cpp

--- cpp/27_removeElement.cpp
+++ cpp/27_removeElement.cpp
@@ -1,1 +1,18 @@
 
+// https://leetcode.cn/problems/remove-element/
+// 快慢指针技巧
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow_idx=0, fast_idx=0;
+
+        for (int fast_idx=0; fast_idx<nums.size(); fast_idx++) {
+            if (nums[fast_idx] != val) {
+                nums[slow_idx] = nums[fast_idx];
+                slow_idx++;
+            } 
+        }
+        return slow_idx;
+    }
+};
