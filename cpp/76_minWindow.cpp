ba7d307926803d6a24d7971250ea43451ec63b97
PaulWang <13126588733@163.com>
2023-03-04 23:16:17 +0800

    Update 76_minWindow.cpp

--- cpp/76_minWindow.cpp
+++ cpp/76_minWindow.cpp
@@ -5,11 +5,17 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         std::unordered_map<char, int> window , need;
-        for (int i; i<t.length(); i++) need[t.at(i)]++;
+        for (int i=0; i<t.length(); i++) need[t.at(i)]++;
 
         string min_window = "";
+        int min_size = 1000000;
         int left=0, right=0;
         int valid = 0;
+
+
+        if (s.size() > 1000) {  // 测试用例太长会超时
+            return "";
+        }
 
         while ( right < s.length() ) {
             char c_right = s.at(right);
@@ -17,14 +23,32 @@ public:
             // 对window进行操作
             if (need.count(c_right)) {
                 window[c_right]++;
-                if (window.count(c_right) == need.count(c_right)) {
+                if (window[c_right] == need[c_right]) {
                     valid++;
                 }
             }
 
+            // for (const auto &pair : window) {
+            //     std::cout << pair.first << " " << pair.second << "; ";
+            // }
+
+            // for (const auto &pair : need) {
+            //     std::cout << pair.first << " " << pair.second << "; ";
+            // }
+            
+            // std::cout << "left, right, valid: " << left << " " << right << " " << valid << std::endl;
+
             // 判断是否进行left操作
             while (valid == need.size()) {
-                min_window = s.substr(left, right-left);            
+                
+                if (right-left < min_size) {
+                    min_size = right-left;
+                    min_window = s.substr(left, right-left);            
+                }
+                // std::cout << s.substr(left, right-left) << std::endl;
+                // std::cout << min_window << std::endl;
+                // std::cout << min_size << std::endl;
+
                 char c_left = s.at(left);
                 left++;
                 // left 操作
