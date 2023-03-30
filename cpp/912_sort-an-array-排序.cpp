34007affb63ce44f85e75f272059deb3e2cacd85
PaulWang <13126588733@163.com>
2023-03-30 22:38:32 +0800

    Update 912_sort-an-array-排序.cpp

--- cpp/912_sort-an-array-排序.cpp
+++ cpp/912_sort-an-array-排序.cpp
@@ -75,3 +75,112 @@ public:
 
     }
 };
+
+// 堆排序
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// class MinHeap {
+//     private:
+//         vector<int> heap_v;
+//     public:
+//         MinHeap(vector<int> heap_v) : heap_v(heap_v) {
+//             auto_adjust();
+//         }
+
+//         void print() {
+//             for (auto item : heap_v) {
+//                 cout << item << " ";
+//             }
+//             cout << endl;
+//         }
+
+//         int left(int idx) {
+//             return idx*2 + 1;
+//         }
+
+//         int right(int idx) {
+//             return idx*2 + 2;
+//         }
+
+//         int parent(int idx) {
+//             return (int)((idx-1)/2);
+//         }
+
+//         bool less(int min_idx, int cmp_idx) {
+//             return heap_v[min_idx] < heap_v[cmp_idx];
+//         }
+
+//         // 交换
+//         void swap(int source_idx, int target_idx) {
+//             int tmp = heap_v[target_idx];
+//             heap_v[target_idx] = heap_v[source_idx];
+//             heap_v[source_idx] = tmp;
+//         }
+
+//         // 下沉
+//         void sink(int node_idx) {
+//             while (left(node_idx) < heap_v.size()) {
+//                 // 假设最小的是左节点
+//                 int min = left(node_idx);
+//                 if (right(node_idx)< heap_v.size()  && less(right(node_idx), min)) {
+//                     min = right(node_idx);
+//                 }
+//                 if (!less(node_idx, min)) {
+//                     swap(node_idx, min);
+//                     node_idx = min;
+//                 } else {
+//                     break;
+//                 }
+//             }
+//         }
+
+//         // 上浮
+//         void swim(int node_idx) {
+//             while (parent(node_idx)>=0) {
+//                 if (less(node_idx, parent(node_idx))) {
+//                     swap(node_idx, parent(node_idx));
+//                     node_idx = parent(node_idx);
+//                 } else {
+//                     break;
+//                 }
+//             }
+//         }
+
+//         // 建立最小堆
+//         void auto_adjust() {
+//             vector<int> tmp_heap_v = heap_v;
+//             heap_v.clear();
+//             for (auto item : tmp_heap_v) {
+//                 heap_v.push_back(item);
+//                 swim(heap_v.size()-1);
+//             }
+//         }
+
+//         // 升序排序输出
+//         void increas_seq() {
+//             vector<int> tmp_heap = heap_v;
+            
+//             while (heap_v.size() > 0) {
+//                 swap(heap_v.size()-1, 0);
+//                 cout << heap_v.back() << " ";
+//                 heap_v.pop_back();
+//                 sink(0);
+//                 // print();
+//             }
+//             cout << endl;
+//             heap_v = tmp_heap;
+//         }
+// };
+
+
+// int main() {
+//     vector<int> test_v = {1, 3, 4, 10, 0, 2, 5, 7};
+//     MinHeap min_heap(test_v);
+//     min_heap.print();
+//     min_heap.increas_seq();
+
+//     return 0;
+// }
